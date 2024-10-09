d99a6f9ab20536fd262026462d4c54908b751530
Tom Kaminski <ttkaminski@gmail.com>
2024-10-09 12:10:43 -0500

    Use proper extension class to fix symfony 7.1 deprection notice

--- DependencyInjection/CHCookieConsentExtension.php
+++ DependencyInjection/CHCookieConsentExtension.php
@@ -12,7 +12,7 @@ namespace ConnectHolland\CookieConsentBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class CHCookieConsentExtension extends Extension
 {
